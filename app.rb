44fb3c93c15e036ccfa4452649527a100ebe21eb
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-10-27 05:09:40 +0000

    progress

--- app.rb
+++ app.rb
@@ -2,8 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/square/new") do
+  erb(:homepage)
+end
+
+get("/square/results") do
+  @num = params.fetch("number").to_f
+  @num_squared = @num * @num
+  
+  erb(:square_result)
+end
+
+get("/square_root/new") do
+  erb(:root)
+end
+
+get("/square_root/results") do
+  @num = params.fetch("user_number").to_f
+  @num_square_root = @num ** 0.5
+
+  erb(:root_result)
+end
+
+get("/random/new") do
+
+  
+
+  erb(:random)
+end
+
+get("/payment/new") do
+
+
+  erb(:payment)
 end
